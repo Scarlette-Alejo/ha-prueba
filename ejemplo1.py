3c90c66fb761dcf2adc1d2e55173be535987ca59
Scarlette-Alejo <A01746190@itesm.mx>
2020-10-29 21:56:10 -0600

    Agregar nuevas fncionalidades y archivos

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,6 +1,6 @@
 import cv2 #Biblioteca para el manejo de imágenes
 
-imagen = cv2.imread('venom.jpeg')#leer el archivo  (ORDEN: bgr)
+imagen = cv2.imread('imagen.jpeg')#leer el archivo  (ORDEN: bgr)
 imegen = cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB) #Cambia orden
 print(imagen.shape) #longitud del archivo
 #print(imagen[0,0])#primer pixel
@@ -9,7 +9,7 @@ print(imagen[0][0][0]) #valor en específico
 #modificar tamaño
 imagen = cv2.resize(imagen,(256,256))
 #solo m y l
-cv2.imwrite('resizevenom.jpeg',imagen)
-cv2.imshow('venom',imagen)
+cv2.imwrite('resizeimagen.jpeg',imagen)
+cv2.imshow('imagen',imagen)
 cv2.waitKey(0)
  
